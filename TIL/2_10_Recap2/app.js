3b96b5d198ae063b2dc92f86c7cc0bf82963f0ab
김영훈 <yhkor11@naver.com>
2026-01-22 16:48:00 +0900

    file upload - 2_10_Recap2

---
+++ TIL/2_10_Recap2/app.js
@@ -0,0 +1,19 @@
+const calculator = {
+    add: function(a, b) {
+        console.log(a + b)
+    },
+    minus: function(a, b) {
+        console.log(a - b)
+    },
+    divide: function(a, b) {
+        console.log(a / b)
+    },
+    power: function(a, b) {
+        console.log(a ** b)
+    },
+};
+
+calculator.add(4, 2)
+calculator.minus(4, 2)
+calculator.divide(4, 2)
+calculator.power(4, 2)
