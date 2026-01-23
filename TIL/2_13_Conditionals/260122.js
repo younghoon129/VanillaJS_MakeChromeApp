8d12ee8f7063ffb4a731f3e20f35804bae22cd72
김영훈 <yhkor11@naver.com>
2026-01-23 17:39:35 +0900

    file upload - 2_13_Conditionals

--- TIL/2_13_Conditionals/260122.js
+++ TIL/2_13_Conditionals/260122.js
@@ -1,3 +1,8 @@
 const age = prompt('How old are you?')
 
 console.log(age)
+console.log(typeof age);
+
+// string 을 number로 변환해줌, 숫자 아니면 NaN 반환
+parseInt('15')
+console.log(typeof "15", typeof parseInt('15'));
