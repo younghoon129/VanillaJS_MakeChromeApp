59ca293cebb24c1865aef88ff0a354322228b126
김영훈 <yhkor11@naver.com>
2026-01-28 16:30:02 +0900

    file upload - HTMLinJavascript

---
+++ TIL/3_1_HTMLinJavascript/260128.js
@@ -0,0 +1,11 @@
+const title = document.getElementById("title");
+
+console.log(title);
+console.dir(title);
+
+title.innerText = "Got you!"
+
+console.log(title.id);
+console.log(title.className);
+
+
