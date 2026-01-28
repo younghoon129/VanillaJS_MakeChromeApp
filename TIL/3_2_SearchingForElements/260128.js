20295ea2de151166cc2c4a07a5bfa404cb198b12
김영훈 <yhkor11@naver.com>
2026-01-28 17:02:45 +0900

    file upload - SearchingForElements

---
+++ TIL/3_2_SearchingForElements/260128.js
@@ -0,0 +1,6 @@
+// const hellos = document.getElementsByClassName('hello');
+// const title = document.getElementsByTagName('h1');
+const title = document.querySelector('.hello h1');
+
+ 
+console.log(title)
