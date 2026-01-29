34e773e666bc8f1684d8f4915dd9a4bd876999d3
김영훈 <yhkor11@naver.com>
2026-01-29 14:53:51 +0900

    file upload - CSSinJavascript

---
+++ TIL/3_6_CSSinJavascript/260129.js
@@ -0,0 +1,15 @@
+const h1 = document.querySelector('.hello:first-child h1');
+
+
+function handleTitleClick() {
+    const currentColor = h1.style.color
+    let newColor;
+    if (currentColor === 'blue') {
+        newColor = 'tomato'
+    } else {
+        newColor = 'blue'
+    }
+    h1.style.color = newColor
+}
+
+h1.addEventListener("click", handleTitleClick)
