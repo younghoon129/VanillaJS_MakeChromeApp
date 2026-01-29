f807e2cc4370ba2d3c7c51b5f69097f3b127f78a
김영훈 <yhkor11@naver.com>
2026-01-29 15:41:02 +0900

    file upload - CSSinJavascript2

---
+++ TIL/3_7_CSSinJavascript2/260129.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector('.hello:first-child h1');
+function handleTitleClick() {
+    const clickedClass = 'active';
+    if (h1.className === clickedClass) {
+        h1.className = '';
+    } else {
+        h1.className = clickedClass
+    }
+}
+h1.addEventListener("click", handleTitleClick);
