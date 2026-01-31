fa26ac426e5f18907a2f0392b1651f92f034da83
김영훈 <yhkor11@naver.com>
2026-01-31 18:07:10 +0900

    file upload - Quotes

---
+++ TIL/6_0_Quotes/js/260131_qu.js
@@ -0,0 +1,58 @@
+const quotes = [
+    {
+        quote: 'quote1',
+        author: 'author1',
+    },
+    {
+        quote: 'quote2',
+        author: 'author2',
+    },
+    {
+        quote: 'quote3',
+        author: 'author3',
+    },
+    {
+        quote: 'quote4',
+        author: 'author4',
+    },
+    {
+        quote: 'quote5',
+        author: 'author5',
+    },
+    {
+        quote: 'quote6',
+        author: 'author6',
+    },
+        {
+        quote: 'quote7',
+        author: 'author7',
+    },
+        {
+        quote: 'quote8',
+        author: 'author8',
+    },
+        {
+        quote: 'quote9',
+        author: 'author9',
+    },
+        {
+        quote: 'quote10',
+        author: 'author10',
+    },
+    
+]
+
+const quote = document.querySelector('#quote span:first-child')
+const author = document.querySelector('#quote span:last-child')
+// const qlen = quotes.length
+// const random = Math.floor(Math.random() * qlen)
+// console.log(quotes[random])
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)]
+
+// Math.round() //반올림
+// Math.ceil() //올림
+// Math.floor() //내림
+
+quote.innerText = todaysQuote.quote
+author.innerText = todaysQuote.author
