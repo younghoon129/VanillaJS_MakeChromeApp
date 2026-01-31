55b1e7a1c4cacc08aaa011f1ac986ea969b8fb77
김영훈 <yhkor11@naver.com>
2026-01-31 16:38:17 +0900

    file upload - LoadingUsername

--- TIL/4_6_LoadingUsername/260130.js
+++ TIL/4_6_LoadingUsername/260130.js
@@ -4,14 +4,26 @@ const link = document.querySelector('a');
 const greeting = document.querySelector('#greeting')
 
 const HIDDEN_CLASSNAME = 'hidden'
+const USERNAME_KEY = 'username'
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME)
     const username = loginInput.value;
-    localStorage.setItem('username', username)
+    localStorage.setItem(USERNAME_KEY, username)
+    paintGreetings(username)
+}
+
+function paintGreetings() {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
-loginForm.addEventListener('submit', onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME)
+    loginForm.addEventListener('submit', onLoginSubmit)
+} else {
+    paintGreetings(savedUsername)
+}
