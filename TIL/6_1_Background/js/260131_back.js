45452abe5055553d5092b266a153d7ef6a4a9792
김영훈 <yhkor11@naver.com>
2026-02-01 16:11:56 +0900

    file upload - Background

--- TIL/6_1_Background/js/260131_back.js
+++ TIL/6_1_Background/js/260131_back.js
@@ -3,4 +3,11 @@ const images = [
     '1.jpeg',
 ]
 
-const chosenImage = images[Math.floor(Math.random) * images.length]
+const chosenImage = images[Math.floor(Math.random() * images.length)]
+
+const bgImage = document.createElement('img');
+
+bgImage.src = `img/${chosenImage}`
+
+console.log(bgImage)
+document.body.appendChild(bgImage)
